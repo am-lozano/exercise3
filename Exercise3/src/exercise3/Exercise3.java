af4e409655c931bdb0e6480c4af05af90708f505
am.lozano <am.lozano@106-18.info2>
2024-09-23 13:54:39 +0200

    Exercise3

---
+++ Exercise3/src/exercise3/Exercise3.java
@@ -0,0 +1,8 @@
+package exercise3;
+
+public class Exercise3 {
+	public static void main(String[] args) {
+		//el + en blanco sirve para "encadenar"
+		System.out.println("1 + 2 " + "+ 3" + " = 6");
+	}
+}
